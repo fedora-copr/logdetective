54fa70d0e75ce90d04b8ef8eadd9a9378a367dda
Tomas Tomecek <ttomecek@redhat.com>
2025-01-22 15:19:32 +0100

    logdetective server: disable TLS
    for now, we still dont have everything in place (DNS, server name,
    nginx)
    
    Signed-off-by: Tomas Tomecek <ttomecek@redhat.com>

--- server/gunicorn-prod.config.py
+++ server/gunicorn-prod.config.py
@@ -6,6 +6,6 @@ workers = 2
 timeout = 120
 # write to stdout
 accesslog = '-'
-certfile = "/src/server/cert.pem"
-keyfile = "/src/server/privkey.pem"
-ca_certs = "/src/server/fullchain.pem"
+# certfile = "/src/server/cert.pem"
+# keyfile = "/src/server/privkey.pem"
+# ca_certs = "/src/server/fullchain.pem"
