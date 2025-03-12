b0c5e74e52a826763f922a35e801cfdc945f2dde
Tomas Tomecek <ttomecek@redhat.com>
2025-03-12 14:58:11 +0100

    introduce an inference test case
    Signed-off-by: Tomas Tomecek <ttomecek@redhat.com>

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,5 @@
 import pytest
-from logdetective.utils import compute_certainty
+from logdetective.utils import compute_certainty, initialize_model, process_log
 
 
 @pytest.mark.parametrize("probs", (
@@ -9,3 +9,12 @@ from logdetective.utils import compute_certainty
 def test_compute_certainty(probs):
     """ test compute_certainty and make sure we can use numpy correctly """
     compute_certainty(probs)
+
+
+def test_process_log():
+    # FIXME: parametrize this and mark as slow
+    model = "Mungert/gemma-3-1b-it-gguf"
+    suffix = "q4_k_s.gguf"
+    model = initialize_model(model, filename_suffix=suffix, verbose=5)
+    response = process_log("illegal hardware instruction (core dumped)", model, False)
+    print(response)
