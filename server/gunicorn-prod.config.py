c704fb3d65590948c52b51ba0f3daa5561963da3
Tomas Tomecek <ttomecek@redhat.com>
2025-03-17 10:28:02 +0100

    increase backend API timeout to 10 minutes
    2 minutes is not enough sadly, especially now that we make much more
    snippets
    
    Signed-off-by: Tomas Tomecek <ttomecek@redhat.com>

--- server/gunicorn-prod.config.py
+++ server/gunicorn-prod.config.py
@@ -2,8 +2,9 @@ import os
 bind = f"0.0.0.0:{os.environ['LOGDETECTIVE_SERVER_PORT']}"
 worker_class = "uvicorn.workers.UvicornWorker"
 workers = 2
-# timeout set to 120 - 2 minutes should be enough for one LLM execution in production on a GPU
-timeout = 120
+# timeout set to 600 seconds; with 32 clusters and several runs in parallel, it
+# can take even 10 minutes for a query to complete
+timeout = 600
 # write to stdout
 accesslog = '-'
 # certfile = "/src/server/cert.pem"
