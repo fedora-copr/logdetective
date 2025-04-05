b0d32af54dd7839e79c0c75aa35bf4b27436cc52
sanyukta <sanyukta@vbox.bbrouter>
2025-04-05 20:28:10 +0530

    Refactor: moved prompt templates to YAML and updated loading from constants.py

--- logdetective/constants.py
+++ logdetective/constants.py
@@ -1,61 +1,21 @@
-# pylint: disable=line-too-long
-DEFAULT_ADVISOR = "fedora-copr/Mistral-7B-Instruct-v0.2-GGUF"
+import os
+import yaml
+from functools import lru_cache
 
-PROMPT_TEMPLATE = """
-Given following log snippets, and nothing else, explain what failure, if any, occured during build of this package.
+PROMPTS_YAML_PATH = os.path.join(os.path.dirname(__file__), "prompts.yaml")
 
-Analysis of the snippets must be in a format of [X] : [Y], where [X] is a log snippet, and [Y] is the explanation.
-Snippets themselves must not be altered in any way whatsoever.
+@lru_cache()
+def _load_yaml(path: str = PROMPTS_YAML_PATH) -> dict:
+    """Load YAML content from the given file path."""
+    with open(path, "r", encoding="utf-8") as f:
+        return yaml.safe_load(f)
 
-Snippets are delimited with '================'.
+_yaml_data = _load_yaml()
 
-Finally, drawing on information from all snippets, provide complete explanation of the issue and recommend solution.
+DEFAULT_ADVISOR = _yaml_data.get("default_advisor", "fedora-copr/Mistral-7B-Instruct-v0.2-GGUF")
+SNIPPET_DELIMITER = _yaml_data.get("constants", {}).get("snippet_delimiter", "================")
 
-Snippets:
-
-{}
-
-Analysis:
-
-"""
-
-SUMMARIZE_PROMPT_TEMPLATE = """
-Does following log contain error or issue?
-
-Log:
-
-{}
-
-Answer:
-
-"""
-
-SNIPPET_PROMPT_TEMPLATE = """
-Analyse following RPM build log snippet. Describe contents accurately, without speculation or suggestions for resolution.
-
-Snippet:
-
-{}
-
-Analysis:
-
-"""
-
-PROMPT_TEMPLATE_STAGED = """
-Given following log snippets, their explanation, and nothing else, explain what failure, if any, occured during build of this package.
-
-Snippets are in a format of [X] : [Y], where [X] is a log snippet, and [Y] is the explanation.
-
-Snippets are delimited with '================'.
-
-Drawing on information from all snippets, provide complete explanation of the issue and recommend solution.
-
-Snippets:
-
-{}
-
-Analysis:
-
-"""
-
-SNIPPET_DELIMITER = "================"
+PROMPT_TEMPLATE = _yaml_data["prompts"]["analyze_snippets"]["template"]
+PROMPT_TEMPLATE_STAGED = _yaml_data["prompts"]["staged_analysis"]["template"]
+SUMMARIZE_PROMPT_TEMPLATE = _yaml_data["prompts"]["summarize_log"]["template"]
+SNIPPET_PROMPT_TEMPLATE = _yaml_data["prompts"]["analyze_snippet_only"]["template"]
