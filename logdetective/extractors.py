de7766442195d07a324e4819440936dd299a0b74
Jiri Podivin <jpodivin@redhat.com>
2025-07-02 15:59:40 +0200

    Allow for explicit filtering of log patterns
    Signed-off-by: Jiri Podivin <jpodivin@redhat.com>

--- logdetective/extractors.py
+++ logdetective/extractors.py
@@ -5,7 +5,8 @@ from typing import Tuple
 import drain3
 from drain3.template_miner_config import TemplateMinerConfig
 
-from logdetective.utils import get_chunks
+from logdetective.utils import get_chunks, filter_snippet_patterns
+from logdetective.models import SkipSnippets
 
 LOG = logging.getLogger("logdetective")
 
@@ -13,7 +14,13 @@ LOG = logging.getLogger("logdetective")
 class DrainExtractor:
     """A class that extracts information from logs using a template miner algorithm."""
 
-    def __init__(self, verbose: bool = False, context: bool = False, max_clusters=8):
+    def __init__(
+        self,
+        verbose: bool = False,
+        context: bool = False,
+        max_clusters=8,
+        skip_snippets: SkipSnippets = SkipSnippets({}),
+    ):
         config = TemplateMinerConfig()
         config.load(f"{os.path.dirname(__file__)}/drain3.ini")
         config.profiling_enabled = verbose
@@ -21,11 +28,15 @@ class DrainExtractor:
         self.miner = drain3.TemplateMiner(config=config)
         self.verbose = verbose
         self.context = context
+        self.skip_snippets = skip_snippets
 
     def __call__(self, log: str) -> list[Tuple[int, str]]:
         out = []
         # First pass create clusters
         for _, chunk in get_chunks(log):
+            # Do not process chunks matching any of the excluded patterns
+            if filter_snippet_patterns(chunk, self.skip_snippets):
+                continue
             processed_chunk = self.miner.add_log_message(chunk)
             LOG.debug(processed_chunk)
         # Sort found clusters by size, descending order
@@ -35,6 +46,9 @@ class DrainExtractor:
         # Second pass, only matching lines with clusters,
         # to recover original text
         for chunk_start, chunk in get_chunks(log):
+            # Chunks matching excluded patterns are again disregarded
+            if filter_snippet_patterns(chunk, self.skip_snippets):
+                continue
             cluster = self.miner.match(chunk, "always")
             if cluster in sorted_clusters:
                 out.append((chunk_start, chunk))
