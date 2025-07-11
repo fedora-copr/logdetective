1378e0a485f3ce92d952410e492f1fc55ae5fa88
Stephen Gallagher <sgallagh@redhat.com>
2025-07-11 11:10:31 -0400

    server: Move Exceptions to their own file
    Signed-off-by: Stephen Gallagher <sgallagh@redhat.com>

---
+++ logdetective/server/exceptions.py
@@ -0,0 +1,29 @@
+"""Exception classes for Log Detective server."""
+
+
+class LogDetectiveException(Exception):
+    """Base exception for Log Detective server."""
+
+
+class LogsMissingError(LogDetectiveException):
+    """The logs are missing, possibly due to garbage-collection"""
+
+
+class LogDetectiveKojiException(LogDetectiveException):
+    """Base exception for Koji-related errors."""
+
+
+class UnknownTaskType(LogDetectiveKojiException):
+    """The task type is not supported."""
+
+
+class NoFailedTask(LogDetectiveKojiException):
+    """The task is not in the FAILED state."""
+
+
+class LogDetectiveConnectionError(LogDetectiveKojiException):
+    """A connection error occurred."""
+
+
+class LogsTooLargeError(LogDetectiveKojiException):
+    """The log archive exceeds the configured maximum size"""
