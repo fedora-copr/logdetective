354891df92fe13c6489b2f3424a56d98ae3b7730
Jiri Podivin <jpodivin@redhat.com>
2025-07-15 10:02:49 +0200

    Refactoring emoji processing
    * Forge url is now using enum with additional checks on validity
    * comments are only processed if any are actually retrieved
    * variables were renamed for improved readability
    * merge_requests and projects dictionaries are not filled if new values were to be `None`
    
    Signed-off-by: Jiri Podivin <jpodivin@redhat.com>

--- logdetective/server/emoji.py
+++ logdetective/server/emoji.py
@@ -10,6 +10,7 @@ from logdetective.server.database.models import (
     Comments,
     Reactions,
     GitlabMergeRequestJobs,
+    Forge,
 )
 from logdetective.server.config import LOG
 
@@ -20,9 +21,11 @@ async def collect_emojis(gitlab_conn: gitlab.Gitlab, period: TimePeriod):
     Check only comments created in the last given period of time.
     """
     comments = Comments.get_since(period.get_period_start_time())
-    comments_for_gitlab_connection = [
-        comment for comment in comments if comment.forge == gitlab_conn.url
-    ]
+    comments_for_gitlab_connection = []
+    if comments:
+        comments_for_gitlab_connection = [
+            comment for comment in comments if comment.forge == gitlab_conn.url
+        ]
     await collect_emojis_in_comments(comments_for_gitlab_connection, gitlab_conn)
 
 
@@ -32,8 +35,16 @@ async def collect_emojis_for_mr(
     """
     Collect emoji feedback from logdetective comments in the specified MR.
     """
-    mr_jobs = GitlabMergeRequestJobs.get_by_mr_iid(gitlab_conn.url, project_id, mr_iid)
-    comments = [Comments.get_by_mr_job(mr_job) for mr_job in mr_jobs]
+    comments = []
+    try:
+        url = Forge(gitlab_conn.url)
+    except ValueError as ex:
+        LOG.exception("Attempt to use unrecognized Forge `%s`", gitlab_conn.url)
+        raise ex
+    mr_jobs = GitlabMergeRequestJobs.get_by_mr_iid(url, project_id, mr_iid)
+
+    if mr_jobs:
+        comments = [Comments.get_by_mr_job(mr_job) for mr_job in mr_jobs]
     await collect_emojis_in_comments(comments, gitlab_conn)
 
 
@@ -63,38 +74,40 @@ async def collect_emojis_in_comments(  # pylint: disable=too-many-locals
     Collect emoji feedback from specified logdetective comments.
     """
     projects = {}
-    mrs = {}
+    merge_requests = {}
     for comment in comments:
         mr_job_db = GitlabMergeRequestJobs.get_by_id(comment.merge_request_job_id)
         if not mr_job_db:
             continue
         if mr_job_db.id not in projects:
-            projects[mr_job_db.id] = project = await _handle_gitlab_operation(
+            project = await _handle_gitlab_operation(
                 gitlab_conn.projects.get, mr_job_db.project_id
             )
             if not project:
                 continue
+            projects[mr_job_db.id] = project
         else:
             project = projects[mr_job_db.id]
-        mr_iid = mr_job_db.mr_iid
-        if mr_iid not in mrs:
-            mrs[mr_iid] = mr = await _handle_gitlab_operation(
-                project.mergerequests.get, mr_iid
+        merge_request_iid = mr_job_db.mr_iid
+        if merge_request_iid not in merge_requests:
+            merge_request = await _handle_gitlab_operation(
+                project.mergerequests.get, merge_request_iid
             )
-            if not mr:
+            if not merge_request:
                 continue
+            merge_requests[merge_request_iid] = merge_request
         else:
-            mr = mrs[mr_iid]
+            merge_request = merge_requests[merge_request_iid]
 
         discussion = await _handle_gitlab_operation(
-            mr.discussions.get, comment.comment_id
+            merge_request.discussions.get, comment.comment_id
         )
         if not discussion:
             continue
 
         # Get the ID of the first note
         note_id = discussion.attributes["notes"][0]["id"]
-        note = await _handle_gitlab_operation(mr.notes.get, note_id)
+        note = await _handle_gitlab_operation(merge_request.notes.get, note_id)
         if not note:
             continue
 
