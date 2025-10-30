4a17f76270e5e03a8367f3e40df21f3e3814f439
Jan Matufka <matufjan@gmail.com>
2025-10-30 10:42:32 +0100

    Removing an import that confused linters
    Signed-off-by: Jan Matufka <matufjan@gmail.com>

--- alembic/env.py
+++ alembic/env.py
@@ -6,8 +6,7 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from logdetective.server.database import models
-from logdetective.server.database.base import get_pg_url
+from logdetective.server.database.base import get_pg_url, Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -22,7 +21,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = models.Base.metadata
+target_metadata = Base.metadata
 
 # print("Tables detected by Alembic:", [table for table in target_metadata.tables.keys()])
 
