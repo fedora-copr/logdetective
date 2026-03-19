a7a36bf2b6e4f287be14ab0a324d078490361569
Jiri Podivin <jpodivin@redhat.com>
2026-03-19 18:29:22 +0100

    Removing rated snippet analysis
    Signed-off-by: Jiri Podivin <jpodivin@redhat.com>

--- logdetective/server/utils.py
+++ logdetective/server/utils.py
@@ -17,7 +17,6 @@ from logdetective.remote_log import RemoteLog
 from logdetective.exceptions import RemoteLogError
 from logdetective.server.models import (
     AnalyzedSnippet,
-    RatedSnippetAnalysis,
     BuildLogRequest,
 )
 
@@ -48,58 +47,9 @@ def we_give_up(details: dict):
     raise HTTPException(500, "Request to the inference API failed")
 
 
-def select_relevance(snippet: AnalyzedSnippet) -> float:
-    """Retrieve relevance value from structure, if there is one."""
-    if not isinstance(snippet.explanation, RatedSnippetAnalysis):
-        LOG.exception("Only rated snippets can be ordered by relevance.")
-        raise ValueError
-    return snippet.explanation.relevance
-
-
 def select_line_number(explanation: AnalyzedSnippet) -> int:
     """Returns line number of original snippet."""
     return explanation.line_number
-
-
-def filter_snippets(
-    processed_snippets: List[AnalyzedSnippet], top_k: int
-) -> List[AnalyzedSnippet]:
-    """Filter snippets according to criteria in config while keeping them ordered by line number.
-    If all snippets recieved the same score, return them all.
-    AnalyzedSnippet objects must have `explanation` attribute set to `RatedSnippetAnalysis`,
-    otherwise raise `ValueError`."""
-
-    if top_k >= len(processed_snippets):
-        LOG.warning(
-            "The `top-k` parameter >= number of original snippets, skipping filtering."
-        )
-        return processed_snippets
-
-    # Sorting invokes `select_relevance` which also tests if objects actually
-    # have the score assigned. Otherwise it raises exception.
-    processed_snippets = sorted(processed_snippets, key=select_relevance, reverse=True)
-
-    # Check for failure mode when all snippets have
-    # the same relevance. In such cases there is no point in filtering
-    # and all snippets are returned.
-    max_relevance = processed_snippets[0].explanation.relevance
-    min_relevance = processed_snippets[-1].explanation.relevance
-
-    LOG.info(
-        "Analyzed snippets sorted. Max relevance: %d Min relevance: %e",
-        max_relevance,
-        min_relevance,
-    )
-    if max_relevance == min_relevance:
-        LOG.warning("All snippets recieved the same rating. Filtering disabled.")
-        return processed_snippets
-
-    processed_snippets = processed_snippets[:top_k]
-
-    # Re-sorting snippets by line number
-    processed_snippets = sorted(processed_snippets, key=select_line_number)
-
-    return processed_snippets
 
 
 async def get_artifacts_from_payload(
@@ -129,13 +79,6 @@ async def get_artifacts_from_payload(
     total_payload_len = sum(len(content) for _, content in build_artifacts.items())
     LOG.info("Total artifact size from the obtained payload (in chars): %d", total_payload_len)
     return build_artifacts
-
-
-def construct_final_prompt(formatted_snippets: str, prompt_template: str) -> str:
-    """Create final prompt from processed snippets and csgrep output, if it is available."""
-
-    final_prompt = prompt_template.format(formatted_snippets)
-    return final_prompt
 
 
 def get_version() -> str:
