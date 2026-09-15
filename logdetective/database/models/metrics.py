d3e4b62157d15bece20a9fa8faa4ff79236f43c1
Jiri Podivin <jpodivin@redhat.com>
2026-09-15 16:51:08 +0200

    Include start_time in results
    Signed-off-by: Jiri Podivin <jpodivin@redhat.com>

--- logdetective/database/models/metrics.py
+++ logdetective/database/models/metrics.py
@@ -199,7 +199,7 @@ class AnalyzeRequestMetrics(Base):
                     ),
                 ).label("average_completion_time"),
             )
-            .where(AnalyzeRequestMetrics.request_received_at > start_time)
+            .where(AnalyzeRequestMetrics.request_received_at >= start_time)
             .where(AnalyzeRequestMetrics.request_received_at < end_time)
             .where(AnalyzeRequestMetrics.endpoint == endpoint)
             .group_by(
